9c6896c7af7249a0e43cef710ace80d1442d45be
mubarak <abubakrobasanjo@gmail.com>
2025-05-05 15:48:46 -0700

    invoke header in the layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/layout/Header";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       
       >
+        <Header/>
         {children}
       </body>
     </html>
